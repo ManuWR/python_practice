d58f408c8d83a1e4b1771769c4c864dd04a77231
Manuel Wong <jose.wong@ibm.com>
2025-01-06 17:28:16 -0600

    Python course Day 18

--- python_practice_projs/bonus/bonus17.py
+++ python_practice_projs/bonus/bonus17.py
@@ -1,6 +1,8 @@
 import FreeSimpleGUI as sg
 from converters14 import convert
 
+sg.theme("Black")
+
 feet_label = sg.Text("Enter feet: ")
 feet_input = sg.Input(key="feet")
 
@@ -10,17 +12,27 @@ inch_input = sg.Input(key="inches")
 convert_button = sg.Button("Convert")
 output_label = sg.Text(key="output")
 
+exit_button = sg.Button("Exit")
+exit_label = sg.Text(key="exit")
+
 window = sg.Window("Convertor", 
                    layout=[[feet_label, feet_input],
                            [inch_label, inch_input],
-                           [convert_button, output_label]])
+                           [convert_button, exit_button, output_label]])
 
 while True:
     event, values = window.read()
-    print(event, values)
-    feet = float(values["feet"])
-    inches = float(values["inches"])
-    result = convert(feet, inches)
-    window["output"].update(value=f"{result} m", text_color="orange")
+    match event:
+        case 'Exit':
+            break
+        case sg.WIN_CLOSED:
+            break
+    try:
+        feet = float(values["feet"])
+        inches = float(values["inches"])
+        result = convert(feet, inches)
+        window["output"].update(value=f"{result} m", text_color="orange")
+    except ValueError:
+        sg.popup("Please provide two numbers.")
 
 window.close()
