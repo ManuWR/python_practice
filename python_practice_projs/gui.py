fa4ce00178f6373625a84e1d5de8665811ef759a
Manuel Wong <j.m.w.r@hotmail.com>
2025-01-17 00:32:01 -0600

    add.png comment from gui and web.py created

--- python_practice_projs/gui.py
+++ python_practice_projs/gui.py
@@ -13,7 +13,9 @@ sg.theme("DarkBlue2")
 clock = sg.Text('', key= "clock")
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
-add_button = sg.Button(size=2, image_source="add.png", tooltip="Add Todo", key="Add")
+# add_button = sg.Button(size=2, image_source="add.png", tooltip="Add Todo", key="Add") 
+# add.png file doesn't work, probably bad github repo copy.
+add_button = sg.Button("Add", size=10)
 list_box = sg.Listbox(values=functions.get_todos(), key="todos",
                          enable_events=True, size=[45, 10])
 edit_button = sg.Button("Edit")
@@ -75,6 +77,5 @@ while True:
             window['todo'].update(value=values['todos'][0])
         case sg.WIN_CLOSED:
             break
-
-print("Bye")
+        
 window.close()
